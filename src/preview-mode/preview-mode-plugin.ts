826564bfb3f7933cfc1ae983210527a31882f3f8
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2023-12-11 10:12:50 -0600

    Fixed headers breaking

--- src/preview-mode/preview-mode-plugin.ts
+++ src/preview-mode/preview-mode-plugin.ts
@@ -1,8 +1,23 @@
 import { MarkdownPostProcessor, MarkdownPostProcessorContext } from 'obsidian';
 
 export const quotePostProcessor: MarkdownPostProcessor = function (el: Node, ctx: MarkdownPostProcessorContext) {
-	if ((el instanceof HTMLElement) && (el.nodeType === el.ELEMENT_NODE) && (el.getElementsByTagName('code').length === 0)) {
+	if (isBodyText(el)) {
+		const htmlElement = el as HTMLElement;
 		const regex = /"([^"]*)"/g;
-		el.innerHTML = el.innerHTML.replace(regex, '<span class="token string">"$1"</span>');
+		console.log(htmlElement.innerHTML);
+		htmlElement.innerHTML = htmlElement.innerHTML.replace(regex, '<span class="token string">"$1"</span>');
 	}
 };
+
+function isBodyText(el: Node): el is HTMLElement {
+	if (
+		el instanceof HTMLElement &&
+		el.nodeType === el.ELEMENT_NODE
+	) {
+		// Check if the element contains only a single <p> tag and no other child elements
+		const paragraphs = el.getElementsByTagName('p');
+		return paragraphs.length === 1 && el.children.length === 1 && el.children[0] === paragraphs[0];
+	}
+
+	return false;
+}
