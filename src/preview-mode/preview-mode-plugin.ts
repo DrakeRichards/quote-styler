2109134280fb6c5b9f5be986c79989a0010f0e9f
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2023-12-14 02:12:42 -0600

    Preview mode now working as intended

--- src/preview-mode/preview-mode-plugin.ts
+++ src/preview-mode/preview-mode-plugin.ts
@@ -2,40 +2,91 @@ import { MarkdownPostProcessor, MarkdownPostProcessorContext } from 'obsidian';
 
 export const quotePostProcessor: MarkdownPostProcessor = function (el: Node, ctx: MarkdownPostProcessorContext) {
 	const htmlElement = el as HTMLElement;
-	if (isBodyText(htmlElement)) {
-		processElement(el as HTMLElement);
+	// If the top-level element is a span, that means the block is probably
+	// a DataView query and should be skipped entirely.
+	if (!isSpan(htmlElement)) {
+		replaceTextContent(htmlElement);
 	}
 };
 
-function isBodyText(el: HTMLElement): boolean {
-	const paragraphs = el.getElementsByTagName('p');
-	
+function replaceTextContent(element: HTMLElement) {
 	if (
-		el.nodeType === el.ELEMENT_NODE &&
-		paragraphs.length === 1 &&
-		el.children.length === 1 &&
-		el.children[0] === paragraphs[0]
+		isTextNodeWithQuotes(element)
+		&& isParentAllowed(element)
 	) {
+		const matches = findQuoteMatches(element);
+		if (matches.length > 0) {
+			const fragment = createFragmentFromMatches(element, matches);
+			replaceTextNodeWithFragment(element, fragment);
+		}
+	} else {
+		processChildNodes(element);
+	}
+}
+
+function isTextNodeWithQuotes(element: HTMLElement): boolean {
+	return (
+		element.nodeType === element.TEXT_NODE &&
+		element.nodeValue !== null
+	);
+}
+
+function isParentAllowed(element: HTMLElement): boolean {
+	const allowedParentNodeNames = ['P', 'LI'];
+	if (element.parentNode === null) {
 		return true;
 	}
-
-	return false;
+	return allowedParentNodeNames.includes(element.parentNode.nodeName);
 }
 
-function processElement(el: HTMLElement) {
-	const paragraphs = el.getElementsByTagName('p');
-	if (paragraphs.length === 1) {
-		const paragraph = paragraphs[0];
-		replaceTextContent(paragraph);
-	}
+function isSpan(element: HTMLElement) {
+	return element.nodeName === 'SPAN'
 }
 
-function replaceTextContent(node: Node) {
-	if (node.nodeType === node.TEXT_NODE && node.nodeValue !== null) {
-		const regex = /"([^"]*)"/g;
-		node.nodeValue = node.nodeValue.replace(regex, '"$1"');
-	} else {
-		// Recursively process child nodes
-		node.childNodes.forEach(replaceTextContent);
+function findQuoteMatches(element: HTMLElement): RegExpMatchArray[] {
+	const regex = /"([^"]*)"/g;
+	if (element.nodeValue === null) {
+		return [];
 	}
+	return [...element.nodeValue.matchAll(regex)];
+}
+
+function createFragmentFromMatches(element: HTMLElement, matches: RegExpMatchArray[]): DocumentFragment {
+	const fragment = document.createDocumentFragment();
+	let lastIndex = 0;
+
+	matches.forEach(match => {
+		// Add text before the match
+		if (element.nodeValue === null) { return; }
+		const beforeText = element.nodeValue.slice(lastIndex, match.index !== undefined ? match.index : lastIndex);
+		if (beforeText.length > 0) {
+			fragment.appendChild(document.createTextNode(beforeText));
+		}
+
+		// Add the matched text with a surrounding span
+		const span = document.createElement('span');
+		span.className = 'token string';
+		span.textContent = `"${match[1]}"`;
+		fragment.appendChild(span);
+
+		// Update the lastIndex for the next iteration
+		lastIndex = match.index !== undefined ? match.index + match[0].length : lastIndex;
+	});
+
+	// Add the remaining text after the last match
+	if (element.nodeValue === null) { return fragment; }
+	const afterText = element.nodeValue.slice(lastIndex);
+	if (afterText.length > 0) {
+		fragment.appendChild(document.createTextNode(afterText));
+	}
+
+	return fragment;
+}
+
+function replaceTextNodeWithFragment(element: HTMLElement, fragment: DocumentFragment) {
+	element.parentNode?.replaceChild(fragment, element);
+}
+
+function processChildNodes(element: HTMLElement) {
+	element.childNodes.forEach(replaceTextContent);
 }
